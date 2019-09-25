d810c24383fa65fe9e3eba12696b7dc7d905eb36
Sanad Abdulrahman Omar <sanad_ab@hotmail.com>
2019-09-26 00:10:43 +0300

    Add project files.

---
+++ BouncingBall/frmBouncingBall.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace BouncingBall
+{
+    partial class frmBouncingBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnl = new System.Windows.Forms.Panel();
+            this.lblNotes = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.lblBounces = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.txtVelocity = new System.Windows.Forms.TextBox();
+            this.lblVelocity = new System.Windows.Forms.Label();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.pnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.BackColor = System.Drawing.Color.Black;
+            this.pnl.Controls.Add(this.lblNotes);
+            this.pnl.Controls.Add(this.pic);
+            this.pnl.Controls.Add(this.lblBounces);
+            this.pnl.Location = new System.Drawing.Point(12, 12);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(300, 300);
+            this.pnl.TabIndex = 0;
+            this.pnl.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_Paint);
+            this.pnl.MouseEnter += new System.EventHandler(this.pnl_MouseEnter);
+            this.pnl.MouseLeave += new System.EventHandler(this.pnl_MouseLeave);
+            // 
+            // lblNotes
+            // 
+            this.lblNotes.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNotes.ForeColor = System.Drawing.Color.Yellow;
+            this.lblNotes.Location = new System.Drawing.Point(64, 167);
+            this.lblNotes.Name = "lblNotes";
+            this.lblNotes.Size = new System.Drawing.Size(173, 56);
+            this.lblNotes.TabIndex = 2;
+            this.lblNotes.Visible = false;
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(95, 74);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(90, 90);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            this.pic.Visible = false;
+            // 
+            // lblBounces
+            // 
+            this.lblBounces.AutoSize = true;
+            this.lblBounces.ForeColor = System.Drawing.Color.Yellow;
+            this.lblBounces.Location = new System.Drawing.Point(202, 11);
+            this.lblBounces.Name = "lblBounces";
+            this.lblBounces.Size = new System.Drawing.Size(57, 13);
+            this.lblBounces.TabIndex = 0;
+            this.lblBounces.Text = "Bounces : ";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnNewGame.Location = new System.Drawing.Point(341, 38);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(107, 32);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.MistyRose;
+            this.btnPause.Location = new System.Drawing.Point(341, 86);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(107, 32);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.BackColor = System.Drawing.Color.Ivory;
+            this.btnResume.Location = new System.Drawing.Point(341, 130);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(107, 32);
+            this.btnResume.TabIndex = 3;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = false;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 50;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // txtVelocity
+            // 
+            this.txtVelocity.Location = new System.Drawing.Point(341, 205);
+            this.txtVelocity.Name = "txtVelocity";
+            this.txtVelocity.Size = new System.Drawing.Size(100, 20);
+            this.txtVelocity.TabIndex = 4;
+            this.txtVelocity.Text = "0.5";
+            // 
+            // lblVelocity
+            // 
+            this.lblVelocity.AutoSize = true;
+            this.lblVelocity.Location = new System.Drawing.Point(338, 189);
+            this.lblVelocity.Name = "lblVelocity";
+            this.lblVelocity.Size = new System.Drawing.Size(44, 13);
+            this.lblVelocity.TabIndex = 5;
+            this.lblVelocity.Text = "Velocity";
+            // 
+            // Logo
+            // 
+            this.Logo.Image = global::BouncingBall.Properties.Resources.Cool_Text_336681621015321;
+            this.Logo.Location = new System.Drawing.Point(-79, 325);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(618, 200);
+            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Logo.TabIndex = 6;
+            this.Logo.TabStop = false;
+            // 
+            // frmBouncingBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.Logo);
+            this.Controls.Add(this.lblVelocity);
+            this.Controls.Add(this.txtVelocity);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.pnl);
+            this.Name = "frmBouncingBall";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bouncing ball";
+            this.pnl.ResumeLayout(false);
+            this.pnl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Label lblNotes;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label lblBounces;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox txtVelocity;
+        private System.Windows.Forms.Label lblVelocity;
+        private System.Windows.Forms.PictureBox Logo;
+    }
+}
+
---
+++ BouncingBall/frmBouncingBall.cs
@@ -0,0 +1,204 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace BouncingBall
+{
+    public partial class frmBouncingBall : Form
+    {
+        public frmBouncingBall()
+        {
+            InitializeComponent();
+            baseBounds = new Rectangle(50, 50, 50, 15);
+            ballBounds = new Rectangle(0, 0, 25, 25);
+            Subscribe();
+        }
+
+        #region Members area :-
+
+        private SoundPlayer player = new SoundPlayer(AppDomain.CurrentDomain.BaseDirectory + "/bounce.wav");
+        private SoundPlayer player_suc = new SoundPlayer(AppDomain.CurrentDomain.BaseDirectory + "/applause.wav");
+
+        private Rectangle baseBounds, ballBounds;
+
+        private Graphics graph;
+
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        private bool dirRight = true, dirDown = true;
+
+        private double velocity = 0, old_velocity = 0;
+
+        private double g = 0.5;
+
+        private int el_X = 0, el_Y = 0, limitX = 0;
+
+        private int bounce = 0;
+
+        #endregion
+
+        #region Main events area :-
+
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.MouseMove += M_GlobalHook_MouseMove;
+        }
+
+        private void M_GlobalHook_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X >= (this.DesktopLocation.X) && e.X <= (this.DesktopLocation.X + this.Width))
+            {
+                var relativePoint = this.PointToClient(Cursor.Position);
+                drawMyRect(relativePoint.X, e.Y);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            moveBall();
+        }
+
+        private void pnl_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+        }
+
+        private void pnl_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Show();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            dirRight = true;
+            dirDown = true;
+            el_Y = 0;
+            velocity = 0;
+            old_velocity = 0;
+            bounce = 0;
+            pic.Visible = false;
+            lblNotes.Visible = false;
+            pic.Image = null;
+            lblBounces.Text = "";
+            timer.Start();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void pnl_Paint(object sender, PaintEventArgs e)
+        {
+            graph = e.Graphics;
+            graph.FillRectangle(Brushes.Yellow, baseBounds);
+            graph.FillEllipse(Brushes.Red, ballBounds);
+        }
+
+        #endregion
+
+        #region Main Functions area :-
+        private void moveBall()
+        {
+            g = Convert.ToDouble(txtVelocity.Text);
+
+            if (dirRight)
+            {
+                el_X += 5;
+
+                if(el_X >= pnl.Width - 50)
+                {
+                    dirRight = false;
+                }
+            }
+            else
+            {
+                el_X -= 5;
+
+                if (el_X <= 5)
+                    dirRight = true;
+            }
+
+            if(dirDown)
+            {
+                velocity = velocity + g;
+
+                if(velocity == old_velocity)
+                {
+                    timer.Stop();
+                    pic.Visible = true;
+                    lblNotes.Visible = true;
+                    player_suc.Play();
+                    pic.Image = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/win.png");
+                    lblNotes.Text = "You are good at this. " + bounce + " bounces.";
+                    return;
+                }
+
+                el_Y = (el_Y + Convert.ToInt32(velocity)) + Convert.ToInt32(0.5 * g);
+
+                if(el_Y >= pnl.Height - 82)
+                {
+                    if(el_X +25 >= limitX && el_X <= (limitX+50))
+                    {
+                        old_velocity = velocity;
+                        dirDown = false;
+                        bounce++;
+                        lblBounces.Text = "Bounce: " + bounce;
+                        player.Play();
+                    }
+                    else
+                    {
+                        timer.Stop();
+                        pic.Visible = true;
+                        lblNotes.Visible = true;
+                        pic.Image = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/loose.png");
+                        lblNotes.Text = "Ohh...please try again.";
+                    }
+                }
+            }
+            else
+            {
+                velocity = velocity - g;
+                el_Y = (el_Y - Convert.ToInt32(velocity)) - Convert.ToInt32(0.5 * g);
+
+                if(velocity <= 0)
+                {
+                    velocity = 0;
+                    dirDown = true;
+                }
+            }
+
+            drawEllipse(el_X, el_Y);
+            pnl.Invalidate();
+        }
+
+        private void drawMyRect(int x, int y)
+        {
+            if ((x + 70) >= pnl.Width)
+                x = pnl.Width - 70;
+
+            if ((x) <= 5)
+                x = 5;
+
+            limitX = x;
+            baseBounds.Location = new Point(x, pnl.Height - 55);
+        }
+
+        private void drawEllipse(int el_X, int el_Y)
+        {
+            ballBounds.Location = new Point(el_X, el_Y);
+        }
+
+        #endregion
+
+    }
+}
