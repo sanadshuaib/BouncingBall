d8fbc39b96584f2c3d0529c119ee48a45235a371
Sanad Abdulrahman Omar <sanad_ab@hotmail.com>
2019-10-03 20:21:05 +0300

    -

--- BouncingBall/frmBouncingBall.cs
+++ BouncingBall/frmBouncingBall.cs
@@ -39,6 +39,106 @@ namespace BouncingBall
 
         #endregion
 
+        #region Main Functions area :-
+        private void moveBall()
+        {
+            g = Convert.ToDouble(txtVelocity.Text);
+
+            if (dirRight)
+            {
+                el_X += 5;
+
+                if (el_X >= pnl.Width - 50)
+                {
+                    dirRight = false;
+                }
+            }
+            else
+            {
+                el_X -= 5;
+
+                if (el_X <= 5)
+                    dirRight = true;
+            }
+
+            if (dirDown)
+            {
+                velocity = velocity + g;
+
+                if (velocity == old_velocity)
+                {
+                    timer.Stop();
+                    pic.Visible = true;
+                    lblNotes.Visible = true;
+                    player_suc.Play();
+                    pic.Image = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/win.png");
+                    lblNotes.Text = "You are good at this. " + bounce + " bounces.";
+
+                    btnResume.Enabled = false;
+                    btnPause.Enabled = false;
+                    return;
+                }
+
+                el_Y = (el_Y + Convert.ToInt32(velocity)) + Convert.ToInt32(0.5 * g);
+
+                if (el_Y >= pnl.Height - 82)
+                {
+                    if (el_X + 25 >= limitX && el_X <= (limitX + 50))
+                    {
+                        old_velocity = velocity;
+                        dirDown = false;
+                        bounce++;
+                        lblBounces.Text = "Bounce: " + bounce;
+                        player.Play();
+                    }
+                    else
+                    {
+                        timer.Stop();
+                        pic.Visible = true;
+                        lblNotes.Visible = true;
+                        pic.Image = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/loose.png");
+                        lblNotes.Text = "Ohh...please try again.";
+
+                        btnResume.Enabled = false;
+                        btnPause.Enabled = false;
+                    }
+                }
+            }
+            else
+            {
+                velocity = velocity - g;
+                el_Y = (el_Y - Convert.ToInt32(velocity)) - Convert.ToInt32(0.5 * g);
+
+                if (velocity <= 0)
+                {
+                    velocity = 0;
+                    dirDown = true;
+                }
+            }
+
+            drawEllipse(el_X, el_Y);
+            pnl.Invalidate();
+        }
+
+        private void drawMyRect(int x, int y)
+        {
+            if ((x + 70) >= pnl.Width)
+                x = pnl.Width - 70;
+
+            if ((x) <= 5)
+                x = 5;
+
+            limitX = x;
+            baseBounds.Location = new Point(x, pnl.Height - 55);
+        }
+
+        private void drawEllipse(int el_X, int el_Y)
+        {
+            ballBounds.Location = new Point(el_X, el_Y);
+        }
+
+        #endregion
+
         #region Main events area :-
 
         public void Subscribe()
@@ -109,106 +209,6 @@ namespace BouncingBall
             graph = e.Graphics;
             graph.FillRectangle(Brushes.Yellow, baseBounds);
             graph.FillEllipse(Brushes.Red, ballBounds);
-        }
-
-        #endregion
-
-        #region Main Functions area :-
-        private void moveBall()
-        {
-            g = Convert.ToDouble(txtVelocity.Text);
-
-            if (dirRight)
-            {
-                el_X += 5;
-
-                if(el_X >= pnl.Width - 50)
-                {
-                    dirRight = false;
-                }
-            }
-            else
-            {
-                el_X -= 5;
-
-                if (el_X <= 5)
-                    dirRight = true;
-            }
-
-            if(dirDown)
-            {
-                velocity = velocity + g;
-
-                if(velocity == old_velocity)
-                {
-                    timer.Stop();
-                    pic.Visible = true;
-                    lblNotes.Visible = true;
-                    player_suc.Play();
-                    pic.Image = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/win.png");
-                    lblNotes.Text = "You are good at this. " + bounce + " bounces.";
-
-                    btnResume.Enabled = false;
-                    btnPause.Enabled = false;
-                    return;
-                }
-
-                el_Y = (el_Y + Convert.ToInt32(velocity)) + Convert.ToInt32(0.5 * g);
-
-                if(el_Y >= pnl.Height - 82)
-                {
-                    if(el_X +25 >= limitX && el_X <= (limitX+50))
-                    {
-                        old_velocity = velocity;
-                        dirDown = false;
-                        bounce++;
-                        lblBounces.Text = "Bounce: " + bounce;
-                        player.Play();
-                    }
-                    else
-                    {
-                        timer.Stop();
-                        pic.Visible = true;
-                        lblNotes.Visible = true;
-                        pic.Image = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/loose.png");
-                        lblNotes.Text = "Ohh...please try again.";
-
-                        btnResume.Enabled = false;
-                        btnPause.Enabled = false;
-                    }
-                }
-            }
-            else
-            {
-                velocity = velocity - g;
-                el_Y = (el_Y - Convert.ToInt32(velocity)) - Convert.ToInt32(0.5 * g);
-
-                if(velocity <= 0)
-                {
-                    velocity = 0;
-                    dirDown = true;
-                }
-            }
-
-            drawEllipse(el_X, el_Y);
-            pnl.Invalidate();
-        }
-
-        private void drawMyRect(int x, int y)
-        {
-            if ((x + 70) >= pnl.Width)
-                x = pnl.Width - 70;
-
-            if ((x) <= 5)
-                x = 5;
-
-            limitX = x;
-            baseBounds.Location = new Point(x, pnl.Height - 55);
-        }
-
-        private void drawEllipse(int el_X, int el_Y)
-        {
-            ballBounds.Location = new Point(el_X, el_Y);
         }
 
         #endregion
